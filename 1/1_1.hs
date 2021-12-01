dc348f7bf0badfd1f87166a1f786d3f69a647eab
Samuel Trew <samuel.trew@outlook.com>
2021-12-01 23:02:56 +0000

    Finished 1_1

--- 1/1_1.hs
+++ 1/1_1.hs
@@ -1,27 +1,27 @@
 import System.IO
-import Control.Monad
 
+main :: IO()
 main = do
-        let list = []
-        handle <- openFile "1_1.txt" ReadMode
-        contents <- hGetContents handle
-        let singlewords = words contents
-            list = f singlewords
-        calc list
-        hClose handle
+    handle <- openFile "1_1.txt" ReadMode
+    contents <- hGetContents handle
+    let str = words contents
+    let list = readInt str
 
-f :: [String] -> [Int]
-f = map read
+    calc list
+    hClose handle
 
-calc :: [Int] -> Int
-calc list =
-    do
-        let result = helper list 0
-        print (nth (read result :: Integer))
+calc :: [Int] -> IO()
+calc list = print (helper list 0)
 
 helper :: [Int] -> Int -> Int
-helper (x:[]) res = res
+helper [] _ = 0
+helper (_:[]) _ = 0
+helper (x:y:[]) res
+    | y > x = res+1
+    | otherwise = res
 helper (x : x' : xs) res
     | x' > x = helper (x':xs) res+1
     | otherwise = helper (x':xs) res
 
+readInt :: [String] -> [Int]
+readInt = map read
