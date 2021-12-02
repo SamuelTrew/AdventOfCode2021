e60fa0d3d4146b898f35b56cdf3e5a53118fe91a
Samuel Trew <samuel.trew@outlook.com>
2021-12-02 00:30:53 +0000

    Updated 1 to be more simple

--- 1/1_1.hs
+++ 1/1_1.hs
@@ -2,26 +2,19 @@ import System.IO
 
 main :: IO()
 main = do
-    handle <- openFile "1_1.txt" ReadMode
+    handle <- openFile "1.txt" ReadMode
     contents <- hGetContents handle
     let str = words contents
     let list = readInt str
 
-    calc list
+    print (calc list)
     hClose handle
 
-calc :: [Int] -> IO()
-calc list = print (helper list 0)
-
-helper :: [Int] -> Int -> Int
-helper [] _ = 0
-helper (_:[]) _ = 0
-helper (x:y:[]) res
-    | y > x = res+1
-    | otherwise = res
-helper (x : x' : xs) res
-    | x' > x = helper (x':xs) res+1
-    | otherwise = helper (x':xs) res
+calc :: [Int] -> Int
+calc (x : x' : xs)
+    | x' > x = 1 + calc (x':xs)
+    | otherwise = calc (x':xs)
+calc _ = 0
 
 readInt :: [String] -> [Int]
 readInt = map read
--- 1/1_2.hs
+++ 1/1_2.hs
@@ -2,28 +2,26 @@ import System.IO
 
 main :: IO()
 main = do
-    handle <- openFile "1_1.txt" ReadMode
+    handle <- openFile "1.txt" ReadMode
     contents <- hGetContents handle
     let str = words contents
     let list = readInt str
 
+    -- print (helper (tail list) (head(list)+head(tail(list))+list!!2))
     calc list
     hClose handle
 
 calc :: [Int] -> IO()
-calc (x:y:z:rest) = print (helper (y:z:rest) (x+y+z) 0)
-calc _ = print(0)
+calc (x:y:z:rest) = print (helper (y:z:rest) (x+y+z))
+calc _ = print 0
 
-helper :: [Int] -> Int -> Int -> Int
-helper (x:y:z:[]) prev res
-    | x+y+z > prev = res + 1
-    | otherwise = res
-helper (x:y:z:rest) prev res
-    | current > prev = helper (y:z:rest) current res+1
-    | otherwise = helper (y:z:rest) current res
+helper :: [Int] -> Int -> Int
+helper (x:y:z:rest) prev
+    | current > prev = 1 + helper (y:z:rest) current
+    | otherwise = helper (y:z:rest) current
     where
         current = x + y + z
-helper _ _ _ = 0
+helper _ _ = 0
 
 readInt :: [String] -> [Int]
 readInt = map read
