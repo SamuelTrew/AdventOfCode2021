a352646b7960758b54a38b14465cff73b5bf48c3
Samuel Trew <samuel.trew@outlook.com>
2021-12-05 19:11:05 +0000

    Fixed 4_1 with off by 1 error

--- 4/1.hs
+++ 4/1.hs
@@ -1,5 +1,5 @@
 import System.IO
-import Data.List (elemIndex, transpose, foldl')
+import Data.List (elemIndex, transpose, foldl', intersect)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
 
@@ -15,19 +15,24 @@ main = do
     let moves = splitOn "," (head ls)
     let boards = createMatrices (createLines ls)
 
-    let results = [matrixRunner mat moves 5 | mat <- boards]
-    let answers = map fst results
+    let results = [matrixRunner mat moves 4 | mat <- boards]
     let moveIndices = map snd results
-    let bestFinalNumber = foldr1 min moveIndices
+    let bestIndex = foldr1 min moveIndices
+    let bestFinalNumber = read (moves!!bestIndex)
 
-    let indexOfEarliest = fromJust $ (elemIndex bestFinalNumber moveIndices)
+    let indexOfEarliest = fromJust $ (elemIndex bestIndex moveIndices)
     let winningMat = boards!!indexOfEarliest
     let total = foldl' (+) 0 [sum (map read row) | row <- winningMat]
-    let rest = sum (map read (answers!!indexOfEarliest))
-    -- print (earliest)
-    print ((total - rest) * bestFinalNumber)
+    let m = sum (map read (marked winningMat (take (bestIndex+1) moves)))
+
+    print ((total - m) * bestFinalNumber)
 
     hClose handle
+
+
+marked :: Mat -> [String] -> [String]
+marked [] _ = []
+marked (x:xs) moves = (intersect x moves)++(marked xs moves)
 
 
 matrixRunner :: Mat -> [String] -> Int -> ([String], Int)
@@ -35,35 +40,19 @@ matrixRunner mat moves index
     | length valid > 0 = (valid, index)
     | otherwise = matrixRunner mat moves (index+1)
     where
-        valid = validateMatrix mat (take index moves)
+        valid = validateMatrix mat (take (index+1) moves)
 
 
 validateMatrix :: Mat -> [String] -> [String]
 validateMatrix mat currMoves
     | rowTruth = mat!!(fromJust $ (elemIndex True rowSubset))
     | colTruth = mat!!(fromJust $ (elemIndex True colSubset))
-    | ldTruth = leftDiag
-    | rdTruth = rightDiag
     | otherwise = []
     where
         rowTruth = any (==True) rowSubset
         colTruth = any (==True) colSubset
-        ldTruth = subSet leftDiag currMoves
-        rdTruth = subSet rightDiag currMoves
         rowSubset = [subSet row currMoves | row <- mat]
         colSubset = [subSet col currMoves | col <- transpose(mat)]
-        leftDiag = getLDiagonal mat 0
-        rightDiag = getRDiagonal mat 0
-
-
-getLDiagonal :: Mat -> Int -> [String]
-getLDiagonal _ 5 = []
-getLDiagonal mat index = ((mat!!index)!!index):(getLDiagonal mat (index+1))
-
-
-getRDiagonal :: Mat -> Int -> [String]
-getRDiagonal _ 5 = []
-getRDiagonal mat index = ((mat!!index)!!(4-index)):(getLDiagonal mat (index+1))
 
 
 subSet :: [String] -> [String] -> Bool
