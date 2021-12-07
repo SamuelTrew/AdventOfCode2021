8065afd95307f85825cdf419001323c47a583789
Samuel Trew <Samuel.trew@outlook.com>
2021-12-07 15:41:51 +0000

    Used median for part 1, trying avg for part 2

---
+++ 7/1.hs
@@ -0,0 +1,31 @@
+import System.IO
+import Data.List.Split (splitOn)
+import Data.List (sort, foldl')
+
+main :: IO()
+main = do
+   handle <- openFile "7.txt" ReadMode
+   contents <- hGetContents handle
+
+   let positions = map read (splitOn "," contents)
+   let sorted = sort positions
+
+   let best = median sorted
+
+   let moved = map (\x -> abs (x - best)) sorted
+
+   print (foldl' (+) 0 moved)
+
+   hClose handle
+
+
+median :: [Int] -> Int
+median [x] = x
+median xs
+   | odd len = xs !! mid
+   | even len = evenMedian
+   where
+      len = length xs
+      mid = len `div` 2
+      evenMedian = (xs!!(mid-1) + xs!!(mid)) `div` 2
+median _ = 0
