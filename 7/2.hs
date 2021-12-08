885c68ae0df41eead2e853051f9cc0818afb4901
Samuel Trew <Samuel.trew@outlook.com>
2021-12-08 09:41:48 +0000

    Fixed using mean for part 2

--- 7/2.hs
+++ 7/2.hs
@@ -9,23 +9,14 @@ main = do
 
    let positions = map read (splitOn "," contents)
    let sorted = sort positions
-
    let best = sum sorted `div` length sorted
 
-   let moved = map (\x -> abs (((x - best) * ((x+1) - best)) `div` 2)) sorted
+   let moved = map (dist best) sorted
 
    print (foldl' (+) 0 moved)
 
    hClose handle
 
 
-median :: [Int] -> Int
-median [x] = x
-median xs
-   | odd len = xs !! mid
-   | even len = evenMedian
-   where
-      len = length xs
-      mid = len `div` 2
-      evenMedian = (xs!!(mid-1) + xs!!(mid)) `div` 2
-median _ = 0
+dist :: Int -> Int -> Int
+dist best x = abs ((abs (x - best) * (abs (x - best) + 1)) `div` 2)
