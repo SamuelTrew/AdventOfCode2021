36d7f1ac3a95fd03dd04db689a2f2341cb322a06
Samuel Trew <samuel.trew@outlook.com>
2021-12-08 23:41:44 +0000

    Code is written for 8_2 but not fully working

--- 8/2.hs
+++ 8/2.hs
@@ -1,6 +1,6 @@
 import System.IO
 import Data.List.Split (splitOn)
-import Data.List (sort, foldl')
+import Data.List (sort, foldl', elemIndex)
 
 main :: IO()
 main = do
@@ -8,35 +8,63 @@ main = do
    contents <- hGetContents handle
 
    let positions = map (splitOn "|") (lines contents)
+
+   let inputs = map (words . head) positions
+   let sorted = map order inputs
    let codes = map (words . (!!1)) positions
 
-   print (foldl' (+) 0 (map count codes))
+   print $ convertAll sorted codes
 
 
    hClose handle
 
 
-count :: [String] -> Int
-count [] = 0
-count (x:xs)
-   | is1 x = 1 + rest
-   | is4 x = 1 + rest
-   | is7 x = 1 + rest
-   | is8 x = 1 + rest
-   | length x == 6 && is0or6or9 x = 1 + rest
-   | otherwise = rest
+convertAll :: [[String]] -> [[String]] -> [String]
+convertAll (s:ss) (c:cs) = (convert s c):(convertAll ss cs)
+convertAll _ _ = []
+
+
+convert :: [String] -> [String] -> String
+convert sorted codes = concat $ map (\x -> show (elemIndex x sorted)) codes
+
+
+order :: [String] -> [String]
+order inputs = (zero:one:two:three:four:five:six:seven:eight:nine:[])
    where
-      rest = count xs
+   zero = getWithinCodes (is0or6or9 inputs) eight
+   one = getN 2 inputs
+   two = theOther (is2or3or5 inputs) [three, five]
+   three = getFromCodes (is2or3or5 inputs) one
+   four = getN 4 inputs
+   five = getWithinCodes (is2or3or5 inputs) six
+   six = theOther (is0or6or9 inputs) [zero, nine]
+   seven = getN 3 inputs
+   eight = getN 7 inputs
+   nine = getFromCodes (is0or6or9 inputs) four
 
-isN :: Int -> String -> Bool
-isN n s = length s == n
+
+getN :: Int -> [String] -> String
+getN len inputs = head (filter (\x -> length x == len) inputs)
 
 
-is0or6or9 :: String -> Int
-is0or6or9
+getFromCodes :: [String] -> String -> String
+getFromCodes inputs codes = head (filter (\x -> containsChars x codes) inputs)
 
 
-is4 = isN 4
-is7 = isN 3
-is8 = isN 7
-is1 = isN 2
+theOther :: [String] -> [String] -> String
+theOther total found = head (filter (\x -> not (elem x found)) total)
+
+
+getWithinCodes :: [String] -> String -> String
+getWithinCodes inputs codes = head (filter (\x -> containsChars codes x) inputs)
+
+
+containsChars :: String -> String -> Bool
+containsChars big small = all (\x -> x `elem` big) small
+
+
+is0or6or9 :: [String] -> [String]
+is0or6or9 = filter (\x -> length x == 6)
+
+is2or3or5 :: [String] -> [String]
+is2or3or5 = filter (\x -> length x == 5)
